f7c3788ea0d3cb578e0a0402e68828a2720fac10
Sourabh Undale <sourabhundale@gmail.com>
2024-05-25 17:08:52 +0530

    first commit

---
+++ farmstaykop/src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './Hero'
+import Category from './Category'
+import About from './About'
+import Property from './Property'
+import Topfarm from './Topfarm'
+import Commets from './Commets'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Category />
+      <About />
+      <Property />
+      <Topfarm />
+      <Commets />
+    </>
+  )
+}
+
+export default Home
